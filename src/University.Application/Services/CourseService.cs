0a1b6dcc0b37f98afac7fcc048abad2d872f1639
samin sadman <saminsadmanshad@gmail.com>
2026-08-13 23:28:37 +0600

    application layer with service classes

---
+++ src/University.Application/Services/CourseService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Domain.Entities.BaseEntities;
+
+namespace University.Application.Services
+{
+    public class CourseService
+    {
+        public async Task<Course> CreateCourseAsync(Course course)
+        {
+            // Implementation for creating a course
+            throw new NotImplementedException();
+        }
+
+        public async Task<Course> GetCourseByIdAsync(Guid courseId)
+        {
+            // Implementation for retrieving a course by ID
+            throw new NotImplementedException();
+        }
+
+        public async Task<Course> UpdateCourseAsync(Course course)
+        {
+            // Implementation for updating a course
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteCourseAsync(Guid courseId) 
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ src/University.Application/Services/StudentService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using University.Domain.Entities.BaseEntities;
+
+namespace University.Application.Services
+{
+    public class StudentService
+    {
+        //Staff members can operation on CRUD students
+        public async Task<Student> CreateStudentAsync(Student student)
+        {
+            // Implementation for creating a student
+            throw new NotImplementedException();
+        }
+
+        public async Task<Student> GetStudentByIdAsync(Guid studentId)
+        {
+            // Implementation for retrieving a student by ID
+            throw new NotImplementedException();
+        }
+
+        public async Task<Student> UpdateStudentAsync(Student student)
+        {
+            // Implementation for updating a student
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteStudentAsync(Guid studentId) 
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
