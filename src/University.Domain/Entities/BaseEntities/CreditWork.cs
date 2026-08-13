63b76d63fcab3ba941917ec2b22245c8a9cce161
samin sadman <saminsadmanshad@gmail.com>
2026-08-13 19:55:06 +0600

    domain project added with base domains

---
+++ src/University.Domain/Entities/BaseEntities/CreditWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Domain.Entities.BaseEntities
+{
+    public class CreditWork
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int Code { get; set; }
+        public string CreditWorkName 
+        {
+            get
+            {
+                return $"{Title} - {Code}";
+            }    
+        }
+        public string? Description { get; set; }
+    }
+}
---
+++ src/University.Domain/Entities/BaseEntities/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Domain.Entities.BaseEntities
+{
+    public class Student
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Roll { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
+
+    }
+}
