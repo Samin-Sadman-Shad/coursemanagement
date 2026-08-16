9ce228da0e03d16218fc947283f9d757c5383a83
samin sadman <saminsadmanshad@gmail.com>
2026-08-16 18:48:07 +0600

    dependency injection for data layer

---
+++ src/University.Persistance/DI/PersistanceServiceRegistration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Application.Contracts.Persistance;
+using University.Persistance.Context;
+using University.Persistance.Repositories;
+
+namespace University.Persistance.DI
+{
+    public static class PersistanceServiceRegistration
+    {
+        public static IServiceCollection AddPersistanceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<UniversityDbContext>(); //postgres provider added later
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IStudentRepository, StudentRepository>();
+            services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<ICreditWorkRepository, CreditWorkRepository>();
+            services.AddScoped<ICourseEnrollmentRepository, CourseEnrollmentRepository>();
+            services.AddScoped<ICreditWorkEnrollmentRepository, CreditWorkEnrollmentRepository>();
+
+            return services;
+        }
+    }
+}
