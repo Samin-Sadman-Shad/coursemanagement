6a379f0aca7f2e7d11e07d7c7719d7fd7488f09a
samin sadman <saminsadmanshad@gmail.com>
2026-08-18 02:35:20 +0600

    validators for course and creditwork create

---
+++ src/University.Application/Models/DTOs/CourseDTOs/Validators/CreateCourseDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Application.Models.DTOs.CourseDTOs.Validators
+{
+    public class CreateCourseDtoValidator:AbstractValidator<CreateCourseDto>
+    {
+        public CreateCourseDtoValidator()
+        {
+            RuleFor(c => c.CourseTitle)
+                .NotEmpty()
+                .MaximumLength(20)
+                .WithMessage("The {PropertyName} can not excced 20 characters")
+                .Must(title => title.All(char.IsLetterOrDigit))
+                .WithMessage("The {PropertyName} can contains only alphanumeric characters");
+        }
+    }
+}
---
+++ src/University.Application/Models/DTOs/CreditWorkDTOs/Validators/CreateCreditWorkDtoValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Application.Models.DTOs.CreditWorkDTOs.Validators
+{
+    public class CreateCreditWorkDtoValidator:AbstractValidator<CreateCreditWorkDto>
+    {
+        public CreateCreditWorkDtoValidator()
+        {
+            RuleFor(cw => cw.Heading)
+                .NotEmpty()
+                .MaximumLength(10)
+                .WithMessage("{PropertyName} can not exceed {PropertyValue} characters")
+                .Must(heading => heading.All(char.IsLetter))
+                .WithMessage("{PropertyName} can contain only letters");
+
+            RuleFor(cw => cw.Code)
+                .NotEmpty()
+                .WithMessage("{PropertyName} can not be empty");
+
+        }
+    }
+}
