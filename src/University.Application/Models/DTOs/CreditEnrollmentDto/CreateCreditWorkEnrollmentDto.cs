410c3c663efbcea61ed6c86635ac2315ff41a84b
samin sadman <saminsadmanshad@gmail.com>
2026-08-17 17:36:32 +0600

    dtos for junction entity

---
+++ src/University.Application/Models/DTOs/CreditEnrollmentDto/CreateCreditWorkEnrollmentDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Application.Contracts.DTOs;
+using University.Application.Models.DTOs.Common;
+using University.Domain.Entities.BaseEntities;
+
+namespace University.Application.Models.DTOs.CreditEnrollmentDto
+{
+    public class CreateCreditWorkEnrollmentDto:BaseCreateDto
+    {
+        public required CreditWork CreditWork { get; set; }
+        public required Student Student { get; set; }
+
+    }
+}
