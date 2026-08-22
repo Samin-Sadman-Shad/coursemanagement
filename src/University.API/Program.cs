cac1063ec5318ade71a748073b58321f80b151eb
samin sadman <saminsadmanshad@gmail.com>
2026-08-22 16:37:10 +0600

    swagger ui registered

--- src/University.API/Program.cs
+++ src/University.API/Program.cs
@@ -1,5 +1,6 @@
 using University.Application.DI;
 using University.Persistance.DI;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+//register swagger generation service
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "University Management API", Version = "v1" });
+});
+
 builder.Services.AddPersistanceServices();
 builder.Services.AddApplicationServices();
 
@@ -16,7 +24,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //openapi/v1.json
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(ui =>
+    {
+        //access swagger
+        ui.SwaggerEndpoint("/swagger/v1/swagger.json", "University API"); 
+    });
 }
 
 app.UseHttpsRedirection();
