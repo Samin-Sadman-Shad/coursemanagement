06a4f66e73dd86d4228839bc08bdf7a62ddc019a
samin sadman <saminsadmanshad@gmail.com>
2026-08-23 11:43:07 +0600

    dependency update

--- src/University.Identity/Configurations/RoleConfiguration.cs
+++ src/University.Identity/Configurations/RoleConfiguration.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
---
+++ src/University.Identity/Services/AuthService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Application.Contracts.Identity;
+using University.Application.Models.Identity;
+using University.Identity.Models;
+
+namespace University.Identity.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly JwtSettingsOptions _jwtSettings;
+
+        public AuthService(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            JwtSettingsOptions jwtSettings)
+        {
+            
+        }
+        public Task<AuthResponse> Login(AuthRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RegistrationResponse> Register(RegistrationRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
